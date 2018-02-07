cd0224ea78e9fee8bb0134afa94b24dcfb790475
Alex Consunji <a.consunji@knights.ucf.edu>
2018-02-07 15:31:15 -0500

    Add project files.

---
+++ TestApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] a_temp = Console.ReadLine().Split(' ');
+            int[] a = Array.ConvertAll(a_temp, Int32.Parse);
+            int counter = 0;
+            int temp;
+            //swap
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i; j < n; j++)
+                {
+                    if (a[i] > a[j])
+                    {
+                        temp = a[i];
+                        a[i] = a[j];
+                        a[j] = temp;
+                        counter++;
+                    }
+                }
+            }
+            Console.WriteLine("Array is sorted in {0} swaps.", counter);
+            Console.WriteLine("First Element: {0}", a[0]);
+            Console.WriteLine("Last Element: {0}", a[n - 1]);
+        }
+    }
+}
